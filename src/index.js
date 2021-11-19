6de526efd1aec21c4a303cb8beeb25cfcaae1c79
Renato <renatomercadoluna@hotmail.com>
2021-11-18 22:30:35 -0500

    refactor assets 📁

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const userPath = require('path');
+
 const characters = [
   'Andy Von De Oniyate',
   'Laughing Bull',
@@ -12,19 +14,21 @@ const characters = [
 const quote = () => {
   const character = characters[Math.floor(Math.random() * characters.length)];
   const assetName = character.replace(/\ .*/g, "$'").toLowerCase();
-  var quotes = require(`./q_${assetName}.json`);
+  const dirPath = userPath.join(__dirname, '/assets/quotes');
+  var quotes = require(`${dirPath}/q_${assetName}.json`);
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
   return `${quote} - ${character}`;
 };
 
 const image = async (path) => {
   const phrase = quote().replace('-', '\n');
-  var quotes = require(`./assets/quotes/q_${assetName}.json`);
+  const dirPath = userPath.join(__dirname, '/assets');
+  var quotes = require(`${dirPath}/quotes/q_${assetName}.json`);
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
   let maxWidth = 200;
   let maxHeight = 50;
   const Jimp = require('jimp');
-  const image = await Jimp.read(`./assets/img/${quote}.jpg`);
+  const image = await Jimp.read(`${dirPath}/img/${quote}.jpg`);
   const font = await Jimp.loadFont(Jimp.FONT_SANS_16_WHITE);
   image.print(
     font,
